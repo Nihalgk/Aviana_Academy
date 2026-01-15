4c838b59edb45d922da44ac31fb072c637afe36f
Nihalgk <nihalgagan07@gmail.com>
2026-01-15 15:58:11 +0530

    fix: remove outdated prerender plugin and add google verification

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import prerender from 'vite-plugin-prerender'
 
 export default defineConfig({
   plugins: [
-    react(),
-    prerender({
-      staticDir: path.join(process.cwd(), 'dist'),
-      routes: ['/', '/our-services', '/corporate-training', '/institutional-training', '/individual-coaching', '/about-us'],
-    }),
+    react()
   ],
   resolve: {
     alias: {
       "@": path.resolve(process.cwd(), "./src"),
     },
   },
+  build: {
+    chunkSizeWarningLimit: 1600,
+  }
 })
