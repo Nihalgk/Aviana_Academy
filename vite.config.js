dda2aabafea5cc27d1fbfddcef54c54824271d0f
Nihalgk <nihalgagan07@gmail.com>
2026-01-15 15:00:44 +0530

    fix: sync lockfile

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { vitePrerender } from 'vite-plugin-prerender'
+import prerender from 'vite-plugin-prerender'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    vitePrerender({
+    prerender({
       staticDir: path.join(__dirname, 'dist'),
       routes: ['/', '/our-services', '/corporate-training', '/institutional-training', '/individual-coaching', '/about-us'],
     }),
